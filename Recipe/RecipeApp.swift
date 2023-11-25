ef14ecc5eac5d2758d8d452acc7b592b40e0481b
Kyoka-Yamamoto <annforstudyabroad@gmail.com>
2023-11-24 21:44:54 -0400

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Kyoka Yamamoto on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
